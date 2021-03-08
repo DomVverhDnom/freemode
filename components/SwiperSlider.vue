5282efeed1cbb63000636b0016dd0739ca151904
Vladysha Iorin <vladislav.jorin@gmail.com>
2021-03-08 22:54:10 +0300

    Свайпер

---
+++ components/SwiperSlider.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="swiper-container">
+      <div :style="`transform: translate3d(${translate}px, 0px, 0px);`" class="swiper-wrapper">   
+          <div @mouseup="checkTranslate" class="swiper-slide">Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide  Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide  Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide 1</div>    
+      </div>
+  </div>
+</template>
+
+<script>
+import Swiper, { Thumbs } from 'swiper'
+import 'swiper/swiper-bundle.css'
+
+Swiper.use([ Thumbs ])
+
+export default {
+   data() {
+      return {
+        thumbsSwiper: null,
+        translate: 300,
+      };
+    },
+    methods:{
+      checkTranslate(evt){
+        console.log(evt)
+      }
+    },
+    mounted() {
+      new Swiper('.swiper-container', {
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true,
+        },
+        
+      })
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="header">
+     <div class="container">
+        <div class="topWrapper">
+            <div class="feedback">Обратная связь</div>
+            <div class="logo">ЛОГО</div>
+            <div class="number">89383402819</div>
+          </div>
+        <div class="botWrapper">
+            <div class="funtions">заглушка</div>
+          <div class="swiper-container">
+              <div class="swiper-wrapper">   
+                <div  class="swiper-slide">Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide  Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide  Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1 Slide 1Slide 1</div>    
+              </div>
+          </div>
+            <div class="number">заглушка</div>
+        </div>
+    
+     </div>
+    </div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+import 'swiper/swiper-bundle.css'
+
+export default {
+    mounted() {
+      new Swiper('.swiper-container', {
+        scrollbar: {
+          el: '.swiper-scrollbar',
+          draggable: true,
+        },
+        slidesPerView: 0.3,
+        freeMode: true,
+        
+      })
+    }
+}
+</script>
+
+<style>
+.container{
+  max-width: 1280px;
+  margin: 0 auto;
+}
+.topWrapper, 
+.botWrapper {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+}
+.swiper-container{ 
+  max-width: 500px;
+  cursor: pointer;
+}
+
+
+
+</style>
